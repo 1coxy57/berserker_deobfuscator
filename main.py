858584f50cc0ac68f5a28df34a6829aa9298f2b7
1coxy57 <158783816+1coxy57@users.noreply.github.com>
2025-01-12 21:06:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import argparse, asyncio
-from berk import decrypt
+from berserker import decrypt
+import os, sys
+import logging
+import colorlog
+
+
+formatter = colorlog.ColoredFormatter("%(log_color)s%(asctime)s - %(levelname)s - %(message)s")
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.setLevel(level=logging.INFO)
+logger.addHandler(handler)
 
 async def main(file: str) -> str:
-    with open(file,'r') as f:
-        content = f.read()
-        res = await decrypt(content)
-        return res
+    try: 
+        with open(file,'r',encoding="utf-8") as f:
+            content = f.read()
+            res = await decrypt(content)
+            return res
+    except:
+        logger.error('An error occured while trying to decrypt the file')
+        sys.exit(0)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="berserker deobf", usage="%(prog)s [options]", description="Berserker Deobfuscator")
+    parser = argparse.ArgumentParser(prog="berserker deobf by coxy57", usage="%(prog)s [options]", description="Berserker Deobfuscator")
     parser.add_argument('file',type=str,help='File you want to deobfuscate')
     args = parser.parse_args()
-    
-    result = asyncio.run(main(args.file))
-    print('decoded', result)
-    open('out.py','w').write(result)
+
+    if not os.path.exists(args.file):
+        logger.warning('File does not exist.')
+        sys.exit(0)
+
+    try:
+        result = asyncio.run(main(args.file))
+        logger.log('Successfully deobfuscated and wrote to out.py')
+        open('out.py','w').write(result)
+    except Exception as e:
+        logging.error('Error: ' + e)
+        sys.exit(0)
     
